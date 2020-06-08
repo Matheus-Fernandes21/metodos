557a427030de6f50ce5516aa0ca3787319e71ff0
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-06-08 14:26:28 -0300

    Add files via upload

---
+++ metodo.java
@@ -0,0 +1,32 @@
+public class metodo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int numx,numy;
+          int resultado =multiplicacao(7,15);
+          
+    		 
+	     System.out.println(resultado);
+	
+	
+	}
+	
+	
+public static int multiplicacao(int numx,int numy) {
+
+	
+	return numx*numy;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
