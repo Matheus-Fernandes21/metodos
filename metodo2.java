e3e1e3867e103d8375276d1aabf886efbadcb65b
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-06-08 14:26:36 -0300

    Add files via upload

---
+++ metodo2.java
@@ -0,0 +1,31 @@
+
+public class metodo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int numx =0;
+	     int numy;
+         int resultado =soma(2,8);
+         
+   		 
+	     System.out.println(resultado);
+	   
+	
+	}
+	
+	
+public static int soma(int numx,int numy) {
+
+	
+	 while(numx < numy){
+         System.out.println(numx);
+         numx++;
+	
+	
+	 
+	 
+}
+	return numx;
+}
+}
