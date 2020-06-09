275ca463e8bf37ccc9a334cdc853186a85151ac0
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-06-08 21:05:17 -0300

    Add files via upload

---
+++ metodo3.java
@@ -0,0 +1,51 @@
+
+public class metodo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	
+		int media = 0;
+		int resultado=media(7,5,6,3);
+   		
+		
+		
+		
+		System.out.println(resultado);
+	 
+		
+		if(media<7);
+		System.out.println("false");
+	
+	} 
+		
+
+	
+	
+
+public static int media(int a,int b,int c,int d){
+
+    System.out.println("media");
+		 
+
+    return (a+b+c+d)/4;
+
+
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
